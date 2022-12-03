97a81d41237927360cf4b6645562a265cfb682db
Joey Harrison <joeyh021@icloud.com>
2022-12-03 16:15:55 +0000

    day 3 golfed

--- src/main/scala/days/Day3.scala
+++ src/main/scala/days/Day3.scala
@@ -3,23 +3,21 @@ package days
 object Day3 extends aoc.Day {
   override def num: Int = 3
 
-  override def solve(input: Seq[String]): (Any, Any) = {
-    val toPriority = (c: Char) => if c.isUpper then c.toInt - 38 else c.toInt - 96
+  extension (c: Char) {
+    def toPriority = if c.isUpper then c.toInt - 38 else c.toInt - 96
+  }
 
-    val p1 = input.map { l =>
-      val sets         = l.splitAt(l.length() / 2)
-      val intersection = (sets(0) intersect sets(1)).distinct
-      toPriority(intersection.head)
-    }.sum
-
-    val p2 =
+  override def solve(input: Seq[String]): (Any, Any) =
+    (
+      input
+        .map(l => l.splitAt(l.size / 2))
+        .map((_ intersect _))
+        .map(_.head.toPriority)
+        .sum,
       input
         .grouped(3)
-        .map(g => g.foldLeft(((('a' to 'z') ++ ('A' to 'Z'))))((s, acc) => (s intersect acc).distinct))
-        .map(s => toPriority(s.head))
+        .map(_.reduce(_ intersect _).head.toPriority)
         .sum
-
-    (p1, p2)
-  }
+    )
 
 }
