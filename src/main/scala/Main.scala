b2fbec12fdaad441b94b30bd626dbeac7858f7f5
Joey Harrison <joeyh021@icloud.com>
2022-12-04 20:43:48 +0000

    default to today

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,24 @@
 package aoc
 
-@main def AoC(n: Int) = println(
+import java.time.LocalDate
+import scala.util.CommandLineParser.FromString
+
+lazy val isDecember22 = LocalDate.now().getYear() == 2022 && LocalDate.now().getMonthValue() == 12
+lazy val today        = LocalDate.now().getDayOfMonth()
+
+def runDay(d: Int): Option[String] =
   solvedDays
-    .lift(n)
+    .lift(d)
     .map { day =>
-      val (p1, p2) = day.run(n); s"Part 1: $p1\nPart 2: $p2"
-    } getOrElse "Day unimplemented!"
+      val (p1, p2) = day.run(d); s"Part 1: $p1\nPart 2: $p2"
+    }
+
+@main def AoC(args: Int*) = println(
+  if args.size > 1 then "Specify a single day to run"
+  else if args.isEmpty && !isDecember22 then "Cannot default to current day, specify a day to run"
+  else if args.size == 1 then runDay(args.head) getOrElse "That day has not been solved!"
+  else runDay(today) getOrElse "You have not solved today's puzzle yet!"
 )
 
 import days._
-def solvedDays = Vector(Day0, Day1, Day2, Day3, Day4)
+val solvedDays = Vector(Day0, Day1, Day2, Day3, Day4)
