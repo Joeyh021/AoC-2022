0d0590367c7757637d757b9e4d3b3aea6b87fa5e
Joey Harrison <joeyh021@icloud.com>
2022-12-06 14:36:20 +0000

    use indexWhere

--- src/main/scala/days/Day6.scala
+++ src/main/scala/days/Day6.scala
@@ -2,5 +2,5 @@ package days
 
 object Day6 extends aoc.Day:
   override def solve(input: String): (Any, Any) =
-    val f = (l: Int) => input.iterator.sliding(l).map(s => s.distinct.size == s.size).zipWithIndex.find(_._1).get._2 + l
+    val f = (l: Int) => input.iterator.sliding(l).map(s => s.distinct.size == s.size).indexWhere(identity) + l
     (f(4), f(14))
