4bcaf6c26a91a25082b53511a71f082a4a719635
Joey Harrison <joeyh021@icloud.com>
2022-12-08 10:53:51 +0000

    tidy up day7 a little

--- src/main/scala/days/Day7.scala
+++ src/main/scala/days/Day7.scala
@@ -4,18 +4,11 @@ import scala.util.matching._
 import scala.collection.mutable.{ ArrayBuffer, HashMap, Stack }
 
 object Day7 extends aoc.Day:
-
-  enum Tree:
-    case Dir(name: String, contents: List[Tree])
-    case File(size: Int)
-
-  import Tree._
-
   enum ParsedLine:
     case Cd(name: String)
     case CdUp
     case Ls
-    case FileInfo(size: Int, name: String)
+    case FileInfo(size: Int)
     case DirInfo(name: String)
 
   import ParsedLine._
@@ -23,12 +16,12 @@ object Day7 extends aoc.Day:
   def parseLine(line: String) =
     val filepat = "([0-9]+) ([a-z]+.[a-z]+)".r
     line match
-      case "$ ls"              => Ls
-      case "$ cd .."           => CdUp
-      case s"$$ cd $dirname"   => Cd(dirname)
-      case s"dir $dirname"     => DirInfo(dirname)
-      case filepat(size, name) => FileInfo(size.toInt, name)
-      case l                   => throw Exception(s"Could not parse input: $l")
+      case "$ ls"            => Ls
+      case "$ cd .."         => CdUp
+      case s"$$ cd $dirname" => Cd(dirname)
+      case s"dir $dirname"   => DirInfo(dirname)
+      case filepat(size, _)  => FileInfo(size.toInt)
+      case l                 => throw Exception(s"Could not parse input: $l")
 
   extension (s: Stack[String]) {
     def toPath: String = s.reverse.foldLeft("")((acc, s) => acc + "/" + s)
@@ -40,13 +33,14 @@ object Day7 extends aoc.Day:
     var dirs      = HashMap[String, ArrayBuffer[String | Int]](("/root", ArrayBuffer()))
     var dir_stack = Stack("root")
 
-    for (line <- parsed)
-      line match
-        case ParsedLine.Cd(name)          => dir_stack.push(name); dirs.addOne((dir_stack.toPath, ArrayBuffer()))
-        case ParsedLine.CdUp              => dir_stack.pop
-        case ParsedLine.Ls                => ()
-        case ParsedLine.FileInfo(size, _) => dirs.get(dir_stack.toPath).get.addOne(size)
-        case ParsedLine.DirInfo(name)     => dirs.get(dir_stack.toPath).get.addOne(dir_stack.toPath + "/" + name)
+    parsed.foreach {
+      _ match
+        case ParsedLine.Cd(name)       => dir_stack.push(name); dirs.addOne((dir_stack.toPath, ArrayBuffer()))
+        case ParsedLine.CdUp           => dir_stack.pop
+        case ParsedLine.Ls             => ()
+        case ParsedLine.FileInfo(size) => dirs.get(dir_stack.toPath).get.addOne(size)
+        case ParsedLine.DirInfo(name)  => dirs.get(dir_stack.toPath).get.addOne(dir_stack.toPath + s"/$name")
+    }
 
     def getDirSize(name: String): Int =
       dirs
@@ -58,8 +52,8 @@ object Day7 extends aoc.Day:
         )
         .sum
 
-    val p1        = dirs.map((k, v) => getDirSize(k)).filter(_ <= 100000).sum
-    val usedSpace = getDirSize("/root")
-    val p2        = dirs.toList.map((k, v) => getDirSize(k)).filter(_ > usedSpace - 40000000).min
+    val sizes = dirs.map((k, v) => getDirSize(k))
+    val p1    = sizes.filter(_ <= 100000).sum
+    val p2    = sizes.filter(_ > getDirSize("/root") - 40000000).min
 
     (p1, p2)
