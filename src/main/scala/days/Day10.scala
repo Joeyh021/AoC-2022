22107e0010aa1b13d5663a45759996a7c24148f5
Joey Harrison <joeyh021@icloud.com>
2022-12-10 20:47:05 +0000

    draw the answer fancy

--- src/main/scala/days/Day10.scala
+++ src/main/scala/days/Day10.scala
@@ -18,13 +18,16 @@ object Day10 extends aoc.Day {
 
     val p1 = List(20, 60, 100, 140, 180, 220).map(i => i * values(i - 1)).sum
 
-    var p2 = ArrayBuffer[Char]()
+    var p2 = ArrayBuffer.fill(42)('█')
+    p2.append('\n')
+
     for (i <- 0 until 6)
+      p2.append('█')
       for (j <- 0 until 40)
         val reg = values(40 * i + j)
         p2.append(if (reg - 1 to reg + 1).contains(j) then ' ' else '█')
-      p2.append('\n')
+      p2.append('█', '\n')
 
-    (p1, p2.prepended('\n').mkString)
+    (p1, p2.prepended('\n').appendedAll(Iterator.fill(42)('█')).mkString)
 
 }
