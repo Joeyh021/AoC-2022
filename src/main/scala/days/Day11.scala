e0b196884d47f4a51f466735074ebd956f192347
Joey Harrison <joeyh021@icloud.com>
2022-12-11 16:56:31 +0000

    day 11 done

--- src/main/scala/days/Day11.scala
+++ src/main/scala/days/Day11.scala
@@ -3,44 +3,63 @@ package days
 import scala.collection.mutable.Buffer
 
 object Day11 extends aoc.Day {
-
   class MonkeyInfo(
-    var items: Buffer[Int],
-    val op: Int => Int,
+    var items: Buffer[Long],
+    val op: Long => Long,
     val test: Int,
     val ifTrue: Int,
     val ifFalse: Int
   ):
-    var inspections = 0
+    var inspections: Long = 0
+
+    override def clone(): MonkeyInfo =
+      MonkeyInfo(
+        this.items.clone(),
+        this.op,
+        this.test,
+        this.ifTrue,
+        this.ifFalse
+      )
+
+  def playWithMonkeys(monkeys: Seq[MonkeyInfo], n: Int, meds: Long => Long) =
+    for (_ <- 1 to n)
+      monkeys
+        .foreach({ monkey =>
+          monkey.items.foreach({ item =>
+            val worry = meds(monkey.op(item))
+            val to    = if worry % monkey.test == 0 then monkey.ifTrue else monkey.ifFalse
+            monkeys(to).items.append(worry)
+            monkey.inspections += 1
+          })
+          monkey.items.clear()
+        })
+    monkeys
+      .sortBy(_.inspections)
+      .reverse
+      .take(2)
+      .map(_.inspections)
+      .reduce(_ * _)
 
   override def solve(input: String): (Any, Any) = {
     val monkeys = input.split("\n\n").map { l =>
       val fields   = l.linesIterator.drop(1).map(_.split(":")(1)).toIndexedSeq
-      val starting = (fields(0).split(",")).map(_.trim.toInt).toBuffer
+      val starting = (fields(0).split(",")).map(_.trim.toLong).toBuffer
       val test     = (fields(2).split(' ')(3).toInt)
       val t        = (fields(3).split(' ')(4).toInt)
       val f        = (fields(4).split(' ')(4).toInt)
       val op = fields(1).split('=')(1).trim match
-        case s"old * old" => (x: Int) => x * x
-        case s"old * $i"  => (x: Int) => x * i.toInt
-        case s"old + $i"  => (x: Int) => x + i.toInt
+        case s"old * old" => (x: Long) => x * x
+        case s"old * $i"  => (x: Long) => x * i.toLong
+        case s"old + $i"  => (x: Long) => x + i.toLong
       MonkeyInfo(starting, op, test, t, f)
     }
 
-    val nRounds = 20
-    for (_ <- 1 to nRounds)
-      monkeys.foreach({ monkey =>
-        monkey.items.foreach({ item =>
-          val worry = monkey.op(item) / 3
-          val to    = if worry % monkey.test == 0 then monkey.ifTrue else monkey.ifFalse
-          monkeys(to).items.append(worry)
-          monkey.inspections += 1
-        })
-        monkey.items.clear()
-      })
+    val p1  = playWithMonkeys(monkeys.toSeq.map(_.clone), 20, (x: Long) => x / 3)
+    val lcm = monkeys.toSeq.map(_.test).reduce(_ * _)
+    val p2  = playWithMonkeys(monkeys.toSeq.map(_.clone), 10000, (x: Long) => x % lcm)
 
-    var p1 = monkeys.sortBy(_.inspections).reverse.take(2).map(_.inspections).reduce(_ * _)
-    (p1, 0)
+    (p1, p2)
+
   }
 
 }
