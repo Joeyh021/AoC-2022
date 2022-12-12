f9629743511d81827a300259463c45d1a6b3e0a4
Joey Harrison <joeyh021@icloud.com>
2022-12-12 18:43:42 +0000

    d12p1

---
+++ src/main/scala/days/Day12.scala
@@ -0,0 +1,55 @@
+package days
+
+import scala.collection.mutable
+object Day12 extends aoc.Day {
+
+  extension (grid: IndexedSeq[IndexedSeq[Char]]) def apply(cs: (Int, Int)) = grid(cs._1)(cs._2)
+
+  def getCoord(c: Char)(using grid: IndexedSeq[IndexedSeq[Char]]) = {
+    val row    = grid.indexWhere(_.contains(c))
+    val column = grid(row).indexWhere(_ == c)
+    (row, column)
+  }
+
+  def getNeighbours(c: (Int, Int))(using g: IndexedSeq[IndexedSeq[Char]]) = {
+    val grid = g.map(_.map(_ match
+      case 'S' => 'a'
+      case 'E' => 'z'
+      case x   => x
+    ))
+
+    val (i, j) = c
+    List((i, j + 1), (i, j - 1), (i - 1, j), (i + 1, j))
+      .filter((i, j) => i >= 0 && i < grid.size && j >= 0 && j < grid(0).size)
+      .filter(cs => grid(cs) <= grid(c) + 1)
+  }
+
+  override def solve(input: String): (Any, Any) = {
+    given grid: IndexedSeq[IndexedSeq[Char]] = input.linesIterator.map(_.iterator.toIndexedSeq).toIndexedSeq
+
+    val start = getCoord('S')
+    val end   = getCoord('E')
+
+    val distances = mutable.Map((start, 0))
+    val queue     = mutable.Queue[(Int, Int)]()
+    val nodes = queue addAll (for {
+      i <- 0 until grid.size
+      j <- 0 until grid(0).size
+    } yield (i, j))
+
+    // dijkstra, badly implemented
+    while !(queue.isEmpty) do {
+      val v = queue.minBy(distances.getOrElse(_, Int.MaxValue))
+      queue.dequeueAll(_ == v)
+
+      for (n <- getNeighbours(v)) {
+        val alt = distances.getOrElse(v, Int.MaxValue) + 1
+        if alt < distances.getOrElse(n, Int.MaxValue) then distances(n) = alt
+      }
+    }
+
+    (distances(end), 0)
+
+  }
+
+}
